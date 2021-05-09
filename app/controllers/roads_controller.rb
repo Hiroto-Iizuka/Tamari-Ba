1e9172c2442d7c6a3e41971409999d93c5535f73
Hiroto-Iizuka <oroxith@gmail.com>
2021-05-10 06:39:11 +0900

    roadのビューとコントローラを作成

--- app/controllers/roads_controller.rb
+++ app/controllers/roads_controller.rb
@@ -1,6 +1,22 @@
 class RoadsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
 
   def new
-    @road = Road.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,5 +1,6 @@
 Rails.application.config.generators do |g|
-  g.stylesheets false
+  g.stylesheets true
   g.skip_routes true
+  g.assets true
   g.helper false
 end
