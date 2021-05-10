5bc307d749cf1891e4f4a6dab34df6bdac795e1a
Hiroto-Iizuka <oroxith@gmail.com>
2021-05-11 08:04:38 +0900

    userテーブルにadminカラムを追加、seedに初期データを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_212524) do
+ActiveRecord::Schema.define(version: 2021_05_10_224823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_05_09_212524) do
     t.integer "engine_size"
     t.integer "experience_years"
     t.boolean "through"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
