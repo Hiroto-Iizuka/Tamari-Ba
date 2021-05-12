05f99f53f428a83f8a94ed8e7eab167d40df465e
Hiroto-Iizuka <oroxith@gmail.com>
2021-05-13 06:23:46 +0900

    newとcreateで投稿機能を実装

--- app/controllers/roads_controller.rb
+++ app/controllers/roads_controller.rb
@@ -1,14 +1,17 @@
 class RoadsController < ApplicationController
   def index
+    @roads = Road.all
   end
 
   def show
   end
 
   def new
+    @road = Road.new
   end
 
   def create
+    Road.create(@road)
   end
 
   def edit
@@ -19,4 +22,5 @@ class RoadsController < ApplicationController
 
   def destroy
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'home#index'
 
-  resources :roads, only: [:index, :show]
+  resources :roads, only: [:index, :new, :create, :show, :edit, :destroy]
     namespace :admin do
       resources :roads, only: [:index, :new, :create, :show, :edit, :destroy]
     end
