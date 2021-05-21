a506b6e0646e9df8a263826398b1dd35f46853b8
Hiroto-Iizuka <oroxith@gmail.com>
2021-05-21 20:48:48 +0900

    新規登録/情報更新に名前欄を作成、ユーザー情報の閲覧ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  get 'users/show' => 'users#show'
   root :to => 'home#index'
 
   resources :roads, only: [:index, :new, :create, :show, :edit, :destroy]
