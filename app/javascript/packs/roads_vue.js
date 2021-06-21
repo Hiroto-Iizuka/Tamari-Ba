3354d8b1e5b4e0967b6fdbc54f8f6c2e44297595
Hiroto-Iizuka <oroxith@gmail.com>
2021-06-21 22:43:11 +0900

    ピンを設置し、ルートを描画

--- app/javascript/packs/roads_vue.js
+++ app/javascript/packs/roads_vue.js
@@ -1,12 +1,3 @@
-import Vue from 'vue'
-import App from '../app.vue'
-import New from './roads/New.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  var app = new Vue({
-    el: "#input-form",
-    data: {
-      adrs_list: []
-    }
-  });
-});
+
+
