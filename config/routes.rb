be28eef26c75f742f9c68a231d3ef73feef563d4
Hiroto-Iizuka <oroxith@gmail.com>
2021-07-08 23:11:50 +0900

    user一覧ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
-  get 'users/edit_bike' => ''
-  get 'users/show' => 'users#show'
+  resources :users, only: [:index, :show]
+
+
+  
   root :to => 'home#index'
 
   resources :roads, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
