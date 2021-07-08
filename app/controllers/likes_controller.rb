1b00bd8ba7377541fb5931a7424ec6b754cb2428
Hiroto-Iizuka <oroxith@gmail.com>
2021-07-08 11:35:08 +0900

    詳細ページのいいねを非同期処理化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
-    current_user.likes.create!(road_id: params[:road_id])
-    redirect_back(fallback_location: root_path)
+    @road = Road.find(params[:road_id])
+    current_user.likes.create!(road_id: @road.id)
   end
 
   def destroy
-    current_user.likes.find_by(road_id: params[:road_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @road = Road.find(params[:road_id])
+    current_user.likes.find_by(road_id: @road.id).destroy!
   end
 end
