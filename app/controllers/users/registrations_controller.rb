2e52c25e2aa9fd16f6c7454dfff954b6cb9d47a3
Hiroto-Iizuka <oroxith@gmail.com>
2021-07-10 13:45:16 +0900

    アバター画像の登録/表示機能を実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -55,7 +55,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.email == "guest@example.com"
       redirect_to root_path, alert: "ゲストユーザーは更新・削除できません"
     else
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :age, :gender, :bike_type, :bike_name, :engine_size, :experience_years, :through, :prefecture_id])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :age, :gender, :bike_type, :bike_name, :engine_size, :experience_years, :through, :prefecture_id, :avatar])
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :prefecture_id)
+      params.require(:user).permit(:name, :prefecture_id, :avatar)
     end
 end
