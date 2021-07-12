fafdf22de7f2ba414e3ffb186a5b70a1a0117902
Hiroto-Iizuka <oroxith@gmail.com>
2021-07-12 12:46:38 +0900

    bootstrap5を実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-
-import 'bootstrap-material-design'
-import '../css/application.scss'
+import 'bootstrap';
+import '../stylesheets/application';
 
 Rails.start()
 Turbolinks.start()
