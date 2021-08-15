99a1e960d8714ceb3bd41b0b3e96a688f5cbb844
Hiroto-Iizuka <oroxith@gmail.com>
2021-08-15 22:10:31 +0900

    deploy.shを編集

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-cd /var/www/Tamari-Ba/ && git pull
-bundle install
+cd /var/www/Tamari-Ba/ && git pull && /home/hiroto/.rbenv/versions/2.7.2/bin/bundle install
 RAILS_ENV=production bundle exec rake assets:precompile
 sudo -s && systemctl restart puma
