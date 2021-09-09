89052de6187083d822f46ba13575bf940b9fc4a1
Hiroto-Iizuka <oroxith@gmail.com>
2021-09-09 21:06:57 +0900

    deploy.shの記述を変更

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,1 @@
-cd /var/www/Tamari-Ba/ && git pull && /home/ec2-user/.rbenv/versions/2.7.2/bin/bundle install
-RAILS_ENV=production bundle exec rake assets:precompile
-sudo systemctl restart puma
+cd /var/www/Tamari-Ba/ && git pull && /home/ec2-user/.rbenv/versions/2.7.2/bin/bundle install && RAILS_ENV=production bundle exec rake assets:precompile && sudo systemctl restart puma
