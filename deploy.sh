dead6bf2cf0610442cf11ecad471d6694eb0436b
Hiroto-Iizuka <oroxith@gmail.com>
2021-09-09 22:06:10 +0900

    deploy.shを編集

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 cd /var/www/Tamari-Ba/ && git pull && /home/ec2-user/.rbenv/versions/2.7.2/bin/bundle install
-RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production /home/ec2-user/.rbenv/versions/2.7.2/bin/bundle exec rake assets:precompile
 sudo systemctl restart puma
